7bfd41d93344962e0982a08b2dc0173a08709d53
huhaifeng <huhaifeng@github.com>
2022-10-26 22:57:10 +0800

    make add default image with e vite import sytax

--- src/libs/helper.ts
+++ src/libs/helper.ts
@@ -1,6 +1,10 @@
 import { UserProps, ColumnProps, ImageProps } from "../type/Types";
 
-function generateFitUrl(
+// 可直接加载静态文件使用
+import columnImage from "../assets/column.jpg";
+import avatarImage from "../assets/avatar.jpg";
+
+export function generateFitUrl(
   data: ImageProps,
   width: number,
   height: number,
@@ -24,11 +28,33 @@ export function addColumnAvatar(
   } else {
     const parseCol = data as ColumnProps;
     data.avatar = {
-      fitUrl: require(parseCol.title
-        ? "@/assets/column.jpg"
-        : "@/assets/avatar.jpg"),
+      //  使用 webpack 的用法
+      // fitUrl: require(parseCol.title ? "@/assets/column.jpg" : "@/assets/avatar.jpg"),
+      //  使用 vite 可以直接加载静态图片
+      fitUrl: parseCol.title ? columnImage : avatarImage,
     };
   }
+}
+
+interface CheckCondition {
+  format?: string[];
+  size?: number;
+}
+
+type ErrorType = "format" | "size" | null;
+
+export function beforeUploadedCheck(file: File, condition: CheckCondition) {
+  const { format, size } = condition;
+  const isValidFormat = format ? format.includes(file.type) : true;
+  const isValidSize = size ? file.size / 1024 / 1024 < size : true;
+  let error: ErrorType = null;
+  if (!isValidFormat) {
+    error = "format";
+  }
+  if (!isValidSize) {
+    error = "size";
+  }
+  return { passed: isValidFormat && isValidSize, error };
 }
 
 // 数组转成一个对象
