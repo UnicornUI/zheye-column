3ffb3c39b1b5c72efd5b333350ed4aeb1ea3b0c9
huhaifeng <huhaifeng@github.com>
2022-10-26 22:50:37 +0800

    change beforerouter method make refresh page can get login info again

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,10 @@ import Login from "./views/Login.vue";
 import Signup from "./views/Signup.vue";
 import CreatePost from "./views/CreatePost.vue";
 import ColumnDetail from "./views/ColumnDetail.vue";
+import PostDetail from "../views/PostDetail.vue";
+import NotFound from "../view/NotFound.vue";
 import store from "./store";
+import { axios } from "./libs/http";
 
 const routerHistory = createWebHistory();
 const router = createRouter({
@@ -25,7 +28,7 @@ const router = createRouter({
       path: "/signup",
       name: "signup",
       component: Signup,
-      meta: { redirectAlreadyLogin: true},
+      meta: { redirectAlreadyLogin: true },
     },
     {
       path: "/column/:id",
@@ -38,13 +41,23 @@ const router = createRouter({
       component: CreatePost,
       meta: { requiredLogin: true },
     },
+    {
+      path: "/posts/:id",
+      name: "post",
+      component: PostDetail,
+    },
+    {
+      path: "/*",
+      name: "notFound",
+      components: NotFound,
+    },
   ],
 });
 
 router.beforeEach((to, from, next) => {
   /*
-   * 第一版本：没有 token 验证的版本
-   */
+  // 1. 第一版本：没有 token 验证的版本
+  // ------------------------------------------------------------
   // 结构对象，使用起来更加的方便
   const { requiredLogin, redirectAlreadyLogin } = to.meta;
   // 判断是否已经登录，没有登录的时候需要重定向到登录界面
@@ -54,6 +67,46 @@ router.beforeEach((to, from, next) => {
     next("/");
   } else {
     next();
+  }
+  */
+
+  // 2. 第二个版本，用户登录信息功能已经完成，token 进行了本地的持久化
+  // -------------------------------------------------------------
+  // vuex 每次刷新页面都会重新刷新构建，这样会使得我们丢失登录信息
+  // 我们借助我们已经 token 信息，重新加载出用户信息
+  const { user, token } = store.state;
+  const { requiredLogin, redirectAlreadyLogin } = to.meta;
+
+  if (!user.isLogin) {
+    if (token) {
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+      store
+        .dispatch("fetchCurrentUser")
+        .then(() => {
+          if (redirectAlreadyLogin) {
+            next("/");
+          } else {
+            next();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          store.commit("logout");
+          next("login");
+        });
+    } else {
+      if (requiredLogin) {
+        next("login");
+      } else {
+        next();
+      }
+    }
+  } else {
+    if (redirectAlreadyLogin) {
+      next("/");
+    } else {
+      next();
+    }
   }
 });
 
